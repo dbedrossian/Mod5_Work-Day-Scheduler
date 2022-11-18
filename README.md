391f67f4ba299a0187c3250a4ab2064fed7e9315
dbedrossian <danny.bedrossian@gmail.com>
2022-11-17 20:39:37 -0500

    Added README

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Mod5_Work-Day-Scheduler
+# Work Day Scheduler
+
+This is a **to-do list app** that organizes your daily schedule and dynamically changes color based on time of day.
+
+## Table of Contents
+
+- [Background](#background)
+- [Install](#install)
+- [Usage](#usage)
+- [Screenshot](#screenshot)
+- [Maintainers](#maintainers)
+- [Contributing](#contributing)
+- [Contributors](#contributors)
+- [License](#license)
+
+## Background
+
+*To use our skills in JQuery and local storage,* we were instructed to create a daily scheduler that organizes each hour in the day. As the hours progress, the line items change from green (future time) to red (present hour) to grey (past time). Each hour has a section to enter a to-do list and then save it to local storage.
+
+
+## Install/Link to Webpage 🔗 
+
+Check out the link to the website:
+
+
+https://dbedrossian.github.io/Mod4_Coding-Quiz/
+
+
+## Usage
+
+This is being used as an assignment project, but could easily be used in real life for any kind of daily scheduler. A real life version might be better if it contained more features, like a weekly option.
+
+
+## Screenshot
+
+![screenshot](./Assets/05-third-party-apis-homework-demo.gif)
+
+## Maintainers
+
+[@Danny Bedrossian](https://github.com/dbedrossian).
+
+## Contributing
+
+Feel free to dive in! [Open an issue](https://github.com/dbedrossian/standard-readme/issues/new) or submit PRs.
+
+
+## Contributors
+
+Figured out this one on my own, with the occasional reference to https://www.geeksforgeeks.org/jquery/ .
+
+
+## License
+
+© Danny Bedrossian
--- script.js
+++ script.js
@@ -1,136 +1,170 @@
-// Date at the top
-var today = dayjs();
-$('#currentDay').text(today.format('dddd, MMMM D, YYYY'));
+$(document).ready(function () {
+  
+  var today = dayjs();
+  $('#currentDay').text(today.format('dddd, MMMM D, YYYY'));
 
+  var currentTime = dayjs().hour();
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  function timeColor(){
+    var element9 = document.getElementById("hour-9");
+    if (currentTime < 9) {
+    element9.classList.add("future")
+    } else if (currentTime === 9) {
+      element9.classList.add("present")
+    } else {
+      element9.classList.add("past")
+    }
+    var element10 = document.getElementById("hour-10");
+    if (currentTime < 10) {
+    element10.classList.add("future")
+    } else if (currentTime === 10) {
+      element10.classList.add("present")
+    } else {
+      element10.classList.add("past")
+    }
+    var element11 = document.getElementById("hour-11");
+    if (currentTime < 11) {
+    element11.classList.add("future")
+    } else if (currentTime === 11) {
+      element11.classList.add("present")
+    } else {
+      element11.classList.add("past")
+    }
+    var element12 = document.getElementById("hour-12");
+    if (currentTime < 12) {
+    element12.classList.add("future")
+    } else if (currentTime === 12) {
+      element12.classList.add("present")
+    } else {
+      element12.classList.add("past")
+    } 
+    var element13 = document.getElementById("hour-1");
+    if (currentTime < 13) {
+    element13.classList.add("future")
+    } else if (currentTime === 13) {
+      element13.classList.add("present")
+    } else {
+      element13.classList.add("past")
+    }
+    var element14 = document.getElementById("hour-2");
+    if (currentTime < 14) {
+    element14.classList.add("future")
+    } else if (currentTime === 14) {
+      element14.classList.add("present")
+    } else {
+      element14.classList.add("past")
+    } var element15 = document.getElementById("hour-3");
+    if (currentTime < 15) {
+    element15.classList.add("future")
+    } else if (currentTime === 15) {
+      element15.classList.add("present")
+    } else {
+      element15.classList.add("past")
+    }
+    var element16 = document.getElementById("hour-4");
+    if (currentTime < 16) {
+    element16.classList.add("future")
+    } else if (currentTime === 16) {
+      element16.classList.add("present")
+    } else {
+      element16.classList.add("past")
+    }
+    var element17 = document.getElementById("hour-5");
+    if (currentTime < 17) {
+    element17.classList.add("future")
+    } else if (currentTime === 17) {
+      element17.classList.add("present")
+    } else {
+      element17.classList.add("past")
+    }
+  
+  }
+  timeColor()
+  
+  
+  $('#hour-9').find('button').on('click', function(){
+    var text9 = $('#hour-9').find('textarea').val();
+    localStorage.setItem("text-9", text9);
+    }
+  )
+  $('#hour-10').find('button').on('click', function(){
+    var text10 = $('#hour-10').find('textarea').val();
+    localStorage.setItem("text-10", text10);
+    }
+  )
+  $('#hour-11').find('button').on('click', function(){
+    var text11 = $('#hour-11').find('textarea').val();
+    localStorage.setItem("text-11", text11);
+    }
+  )
+  $('#hour-12').find('button').on('click', function(){
+    var text12 = $('#hour-12').find('textarea').val();
+    localStorage.setItem("text-12", text12);
+    }
+  )
+  $('#hour-1').find('button').on('click', function(){
+    var text1 = $('#hour-1').find('textarea').val();
+    localStorage.setItem("text-1", text1);
+    }
+  )
+  $('#hour-2').find('button').on('click', function(){
+    var text2 = $('#hour-2').find('textarea').val();
+    localStorage.setItem("text-2", text2);
+    }
+  )
+  $('#hour-3').find('button').on('click', function(){
+    var text3 = $('#hour-3').find('textarea').val();
+    localStorage.setItem("text-3", text3);
+    }
+  )
+  $('#hour-4').find('button').on('click', function(){
+    var text4 = $('#hour-4').find('textarea').val();
+    localStorage.setItem("text-4", text4);
+    }
+  )
+  $('#hour-5').find('button').on('click', function(){
+    var text5 = $('#hour-5').find('textarea').val();
+    localStorage.setItem("text-5", text5);
+    }
+  )
+  
+  
+  function renderLastText() {
+    var lastText9 = localStorage.getItem("text-9");
+    $('#hour-9').find('textarea').text(lastText9);
+  
+    var lastText10 = localStorage.getItem("text-10");
+    $('#hour-10').find('textarea').text(lastText10);
+  
+    var lastText11 = localStorage.getItem("text-11");
+    $('#hour-11').find('textarea').text(lastText11);
+  
+    var lastText12 = localStorage.getItem("text-12");
+    $('#hour-12').find('textarea').text(lastText12);
+  
+    var lastText1 = localStorage.getItem("text-1");
+    $('#hour-1').find('textarea').text(lastText1);
+  
+    var lastText2 = localStorage.getItem("text-2");
+    $('#hour-2').find('textarea').text(lastText2);
+  
+    var lastText3 = localStorage.getItem("text-3");
+    $('#hour-3').find('textarea').text(lastText3);
+  
+    var lastText4 = localStorage.getItem("text-4");
+    $('#hour-4').find('textarea').text(lastText4);
+  
+    var lastText5 = localStorage.getItem("text-5");
+    $('#hour-5').find('textarea').text(lastText5);
+  }
+  
+  function init() {
+    renderLastText();
+  }
+  init();
+  
 });
 
-// WHEN I click into a timeblock, I can enter an event
-// WHEN I click the save button for that timeblock, the text for that event is saved in local storage
-// WHEN I refresh the page, the saved events persist
 
 
-var currentTime = dayjs().hour();
 
-function timeColor(){
-  var element9 = document.getElementById("hour-9");
-  if (currentTime < 9) {
-  element9.classList.add("future")
-  } else if (currentTime === 9) {
-    element9.classList.add("present")
-  } else {
-    element9.classList.add("past")
-  }
-  var element10 = document.getElementById("hour-10");
-  if (currentTime < 10) {
-  element10.classList.add("future")
-  } else if (currentTime === 10) {
-    element10.classList.add("present")
-  } else {
-    element10.classList.add("past")
-  }
-  var element11 = document.getElementById("hour-11");
-  if (currentTime < 11) {
-  element11.classList.add("future")
-  } else if (currentTime === 11) {
-    element11.classList.add("present")
-  } else {
-    element11.classList.add("past")
-  }
-  var element12 = document.getElementById("hour-12");
-  if (currentTime < 12) {
-  element12.classList.add("future")
-  } else if (currentTime === 12) {
-    element12.classList.add("present")
-  } else {
-    element12.classList.add("past")
-  } 
-  var element13 = document.getElementById("hour-1");
-  if (currentTime < 13) {
-  element13.classList.add("future")
-  } else if (currentTime === 13) {
-    element13.classList.add("present")
-  } else {
-    element13.classList.add("past")
-  }
-  var element14 = document.getElementById("hour-2");
-  if (currentTime < 14) {
-  element14.classList.add("future")
-  } else if (currentTime === 14) {
-    element14.classList.add("present")
-  } else {
-    element14.classList.add("past")
-  } var element15 = document.getElementById("hour-3");
-  if (currentTime < 15) {
-  element15.classList.add("future")
-  } else if (currentTime === 15) {
-    element15.classList.add("present")
-  } else {
-    element15.classList.add("past")
-  }
-  var element16 = document.getElementById("hour-4");
-  if (currentTime < 16) {
-  element16.classList.add("future")
-  } else if (currentTime === 16) {
-    element16.classList.add("present")
-  } else {
-    element16.classList.add("past")
-  }
-  var element17 = document.getElementById("hour-5");
-  if (currentTime < 17) {
-  element17.classList.add("future")
-  } else if (currentTime === 17) {
-    element17.classList.add("present")
-  } else {
-    element17.classList.add("past")
-  }
-
-}
-timeColor()
-
-
-// var save9 = document.getElementById("hour-9").getElementsByTagName('button');
-
-// save9.addEventListener("click", function(event) {
-//   event.preventDefault();
-// console.log("click");
-
-//   var email = document.getElementById("hour-9").getElementsByTagName('textarea');
-
-//     localStorage.setItem("email", email);
-//     renderLastRegistered();
-//   }
-// );
-
-$('#hour-9').find('button').on('click', function(){
-  var text = $('#hour-9').find('textarea').val();
-  localStorage.setItem("text", text);
-})
-
-function renderLastText() {
-  var lastText = localStorage.getItem("text");
-  $('#hour-9').find('textarea').text(lastText);
-
-}
-
-function init() {
-  renderLastText();
-}
-init();
